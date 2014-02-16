87be94d03b1aa1234bc4f28aa46f3bbe1f7045d9
jpostdesign <jpost.design@gmail.com>
2014-02-16 11:19:29 -0800

    better notes within picturefill.js file

--- picturefill/picturefill.js
+++ picturefill/picturefill.js
@@ -1,4 +1,24 @@
-/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
+/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Original Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 
+
+//////
+Enhanced by Jason Post, 2014. 
+
+Added JavaScript to add class, ID and title to img element.
+
+Add a class in top parent span that will be added to all span tags with "data-class"
+
+Add specific classes based on child spans with "class"
+
+<span data-class="parentClassApplied">
+    <span class="specificClassOne" ...etc...  ></span>
+    <span class="specificClassTwo" ...etc...  ></span>
+</span>
+
+Add titles with "data-title" and ID with with "data-id"
+
+You can combine top parent class names with specific names too. The script adds them with a space in the middle. From the preceding example, the complete class added to the first image span would be class="parentClassApplied specificClassOne"
+
+*/
 
 (function( w ){
 
